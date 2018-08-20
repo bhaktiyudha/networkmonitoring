404f7752b6a5f160167faab9ab56e533b77080d2
mufti <muftiismirizqi@gmail.com>
2018-08-20 01:46:40 +0000

    update mongodb init setup script

--- conf_mongodb/mongo_setup.js
+++ conf_mongodb/mongo_setup.js
@@ -4,7 +4,7 @@ db.users.insert({
 	uuid : "73a392e8-d3d9-4695-a935-e25ff48e714d",
 	username : "root",
 	password : "10/w7o2juYBrGMh32/KbveULW9jk2tejpyUAD+uC6PE=",
-	roles : "root",x
+	roles : "root",
 	joined_at : ISODate("2018-06-26T09:44:12.242Z")
 });
 db.createUser({
